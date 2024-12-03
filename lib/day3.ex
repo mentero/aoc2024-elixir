d018ebe68d796d42a18667caa3add95fbad6af2b
mentero <xmentero@gmail.com>
2024-12-03 19:31:27 +0100

    Day 3 Part 2 🎉

--- lib/day3.ex
+++ lib/day3.ex
@@ -15,41 +15,84 @@ defmodule AOC2024.Day3 do
     data
     |> Stream.chunk_while(
       {},
-      fn
-        "m", {} ->
-          {:cont, {"m"}}
-
-        "u", {"m"} ->
-          {:cont, {"mu"}}
-
-        "l", {"mu"} ->
-          {:cont, {"mul"}}
-
-        "(", {"mul"} ->
-          {:cont, {"mul("}}
-
-        digit, {"mul("} when digit in @digits ->
-          {:cont, {"mul(", digit}}
-
-        new_digit, {"mul(", arg0} when new_digit in @digits and byte_size(arg0) < 3 ->
-          {:cont, {"mul(", arg0 <> new_digit}}
-
-        ",", {"mul(", arg0} ->
-          {:cont, {"mul(", arg0, ","}}
-
-        digit, {"mul(", arg0, ","} when digit in @digits ->
-          {:cont, {"mul(", arg0, ",", digit}}
-
-        new_digit, {"mul(", arg0, ",", arg1} when new_digit in @digits and byte_size(arg1) < 3 ->
-          {:cont, {"mul(", arg0, ",", arg1 <> new_digit}}
-
-        ")", {"mul(", arg0, ",", arg1} ->
-          {:cont, {String.to_integer(arg0), String.to_integer(arg1)}, {}}
-
-        _, _ ->
-          {:cont, {}}
+      fn char, memory ->
+        case parse_mul(char, memory) do
+          {:cont, memory} -> {:cont, memory}
+          {:ok, {arg0, arg1}} -> {:cont, {arg0, arg1}, {}}
+        end
       end,
       fn {} -> {:cont, {}} end
+    )
+    |> Stream.map(fn {arg0, arg1} -> arg0 * arg1 end)
+    |> Enum.sum()
+  end
+
+  def parse_mul("m", {}), do: {:cont, {"m"}}
+  def parse_mul("u", {"m"}), do: {:cont, {"mu"}}
+  def parse_mul("l", {"mu"}), do: {:cont, {"mul"}}
+  def parse_mul("(", {"mul"}), do: {:cont, {"mul("}}
+  def parse_mul(digit, {"mul("}) when digit in @digits, do: {:cont, {"mul(", digit}}
+
+  def parse_mul(next_digit, {"mul(", arg0}) when next_digit in @digits and byte_size(arg0) < 3,
+    do: {:cont, {"mul(", arg0 <> next_digit}}
+
+  def parse_mul(",", {"mul(", arg0}), do: {:cont, {"mul(", arg0, ","}}
+
+  def parse_mul(digit, {"mul(", arg0, ","}) when digit in @digits,
+    do: {:cont, {"mul(", arg0, ",", digit}}
+
+  def parse_mul(next_digit, {"mul(", arg0, ",", arg1})
+      when next_digit in @digits and byte_size(arg1) < 3,
+      do: {:cont, {"mul(", arg0, ",", arg1 <> next_digit}}
+
+  def parse_mul(")", {"mul(", arg0, ",", arg1}),
+    do: {:ok, {String.to_integer(arg0), String.to_integer(arg1)}}
+
+  # Reset the memory on invalid match
+  def parse_mul(_, _), do: {:cont, {}}
+
+  def parse_do("d", {}), do: {:cont, {"d"}}
+  def parse_do("o", {"d"}), do: {:cont, {"do"}}
+  def parse_do("(", {"do"}), do: {:cont, {"do("}}
+  def parse_do(")", {"do("}), do: :ok
+  def parse_do(_, _), do: {:cont, {}}
+
+  def parse_dont("d", {}), do: {:cont, {"d"}}
+  def parse_dont("o", {"d"}), do: {:cont, {"do"}}
+  def parse_dont("n", {"do"}), do: {:cont, {"don"}}
+  def parse_dont("'", {"don"}), do: {:cont, {"don'"}}
+  def parse_dont("t", {"don'"}), do: {:cont, {"don't"}}
+  def parse_dont("(", {"don't"}), do: {:cont, {"don't("}}
+  def parse_dont(")", {"don't("}), do: :ok
+  def parse_dont(_, _), do: {:cont, {}}
+
+  def part2(data) do
+    data
+    |> Stream.chunk_while(
+      {true, {}, {}},
+      fn
+        char, {true, mul_memory, dont_memory} ->
+          parsed_mul = parse_mul(char, mul_memory)
+          parsed_dont = parse_dont(char, dont_memory)
+
+          case {parsed_mul, parsed_dont} do
+            {{:ok, {arg0, arg1}}, {:cont, _}} ->
+              {:cont, {arg0, arg1}, {true, {}, {}}}
+
+            {{:cont, _}, :ok} ->
+              {:cont, {false, {}, {}}}
+
+            _ ->
+              {:cont, {true, elem(parsed_mul, 1), elem(parsed_dont, 1)}}
+          end
+
+        char, {false, {}, do_memory} ->
+          case parse_do(char, do_memory) do
+            {:cont, memory} -> {:cont, {false, {}, memory}}
+            :ok -> {:cont, {true, {}, {}}}
+          end
+      end,
+      fn _ -> {:cont, nil} end
     )
     |> Stream.map(fn {arg0, arg1} -> arg0 * arg1 end)
     |> Enum.sum()
--- test/day3_test.exs
+++ test/day3_test.exs
@@ -15,6 +15,19 @@ defmodule AOC2024.Day3Test do
     end
   end
 
+  describe "part2/1" do
+    test "example" do
+      example = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+      stream = Day3.input(example)
+      assert Day3.part2(stream) == 48
+    end
+
+    test "input" do
+      stream = File.stream!("inputs/day_3", 1)
+      assert Day3.part2(stream) == 103_811_193
+    end
+  end
+
   defp provided_example do
     "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
   end
